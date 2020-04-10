a280e8dc2174c5f3606be7055672d59b5412def3
Bineesh P BALAN <Bineesh.P.BALAN@sapiens.com>
2020-04-10 12:29:05 +0530

    basic structure design

---
+++ main.js
@@ -0,0 +1,23 @@
+/* author: Bineesh P Balan
+   assignment: diagonal highlights
+*/
+
+window.onload = function() {
+	
+}
+
+function createBoard(size) {
+
+}
+
+function makeMove() {
+
+}
+
+function clear() {
+
+}
+
+function highlightBox() {
+
+}
