6135ea095dc1c78a711e77b1070805092560e374
Bineesh P BALAN <Bineesh.P.BALAN@sapiens.com>
2020-04-10 13:19:33 +0530

    updated chess board color

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ function createBoard(size) {
 	for(var i=0;i<size;i++) {
 		for(var j="0";j<size;j++) {
 		    var append_child = document.createElement(config.append_element)
+		    if(j%2!=0 && i%2==0 || j%2==0 && i%2!=0) {
+		    	append_child.setAttribute("style","background-color:"+config.secondary__color);
+		    }
 			play_board[0].appendChild(append_child);
 		}
 	}
