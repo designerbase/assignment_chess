251bb47c15c0745382bedba9acab7f90c2852721
Bineesh P BALAN <Bineesh.P.BALAN@sapiens.com>
2020-04-10 18:34:28 +0530

    completed

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ var config = {
 window.onload = function() {
 	createBoard();
 	selectBox();
+	document.querySelector('#clear').addEventListener('click',function() {
+		clear();
+	});
 }
 
 function createBoard() {
 	var play_board = document.querySelectorAll(config.board);
+	var index =0;
 	for(var i=0;i<config.size;i++) {
 		for(var j="0";j<config.size;j++) {
-		    var append_child = document.createElement(config.append_element)
+		    var append_child = document.createElement(config.append_element);
+		    var m = [j,i];
+		    append_child.setAttribute("data-index",m);
 		    if(j%2!=0 && i%2==0 || j%2==0 && i%2!=0) {
 		    	append_child.setAttribute("style","background-color:"+config.secondary__color);
 		    }
@@ -33,7 +39,7 @@ function createBoard() {
 function selectBox(e) {
 	if(e) {
 		highlightBox(e.target);
-		chooseDiagonals();
+		chooseDiagonals(e.target);
 	}
 }
 
@@ -46,6 +52,27 @@ function highlightBox(column) {
 	column.setAttribute("style","background-color:red");
 }
 
-function chooseDiagonals() {
-	
+function chooseDiagonals(elem) {
+	console.log(elem);
+	var get_index = [elem.getAttribute('data-index')];
+	get_index = get_index[0].split(',');
+	var x = parseInt(get_index[0]);
+	var y = parseInt(get_index[1]);
+	for(var i=x-1,j=y-1;i>=0 && j>=0 ; i--,j--){
+	    callHighlight([i,j]);
+	}
+	for(var i=x-1,j=y+1;i>=0 && j<8; i--,j++){
+	    callHighlight([i,j]);
+	}
+	for(var i=x+1,j=y-1;i<=7 && j>=0; i++,j--){
+	    callHighlight([i,j]);
+	}
+	for(var i=x+1,j=y+1;i<=7 && j<=7; i++,j++){
+	    callHighlight([i,j]);
+	 }
+}
+
+function callHighlight(data) {
+	var string = '"' + data + '"';
+	highlightBox(document.querySelectorAll("li[data-index="+string+"]")[0]);
 }
